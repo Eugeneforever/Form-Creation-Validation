5b0cfa7d6ab55a30b61301436d4cd4c102b0d569
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2024-10-13 20:39:14 +0000

    html, css and Js files for Userlist

---
+++ fetch-data.js
@@ -0,0 +1,26 @@
+//a promise is an activity when a task is not going to be given to you now but given to you later
+
+async function fetchUserData() {
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById("api-data");
+    try {
+      const response = await fetch(apiUrl);
+      const users = await response.json();
+      dataContainer.innerHTML = "";
+      const userList = document.createElement("ul");
+  
+      users.forEach((element) => {
+        const li = document.createElement("li");
+        li.textContent = element.name;
+        userList.appendChild(li);
+      });
+  
+      dataContainer.appendChild(userList);
+     
+    } catch (error) {
+      console.error(error);
+      dataContainer.innerHTML = '';
+      dataContainer.textContent = 'Failed to load user data.';
+    }
+  }
+  document.addEventListener('DOMContentLoaded',fetchUserData)
